0e7a1783727560c1d49ec831d674282383d4c9cf
xyzkoko <358008599@qq.com>
2019-01-03 09:42:38 +0800

    1

--- config/headimgurl.php
+++ config/headimgurl.php
@@ -5,9 +5,9 @@ return [
     // TODO php artisan config:cache
 
     // 图片保存地址photo目录下
-    'sava_uri' => '/data/wwwroot/nn/public/photo',
+    'sava_uri' => '/data/wwwroot/dx/public/photo',
 
     // 图片访问地址
-    'visit_uri' => 'http://140.143.168.189/nn/public/photo',
+    'visit_uri' => 'http://140.143.168.189/dx/public/photo',
 
 ];
